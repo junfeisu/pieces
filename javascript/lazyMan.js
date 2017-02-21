cba887c00fcbd0e45c9b59f5eabb919149f3dc26
sjf <1982764150@qq.com>
2017-02-21 19:33:42 +0800

    finish the lazyMan

--- javascript/lazyMan.js
+++ javascript/lazyMan.js
@@ -1,36 +1,61 @@
-lazyMan('sujunfei').sleep(100).eat('beaf')
-
-function _lazyMan () {
-  this.task = []
-  this.task.push()
-  return 
-}
-
-_lazyMan.prototype.sayHi = function (name) {
-  console.log('Hi ' + name + ', you are welcome')
+function _lazyMan (name) {
+  var self = this
+  self.task = []
+  var fn = (function (n) {
+    return function () {
+      console.log('Hi ' + n + ', you are welcome')
+      self.next()
+    }
+  })(name)
+  self.task.push(fn)
+  setTimeout(function () {
+    self.next()
+  }, 0) 
 }
 
 _lazyMan.prototype.next = function () {
-  this.task[0]()
-  this.task.shift()
+  var fn = this.task.shift()
+  fn()
 }
  
 _lazyMan.prototype.sleep = function (delay) {
   var self = this
-  setTimeout(function () {
-    self.task[0]()
-  }, delay)
+  var fn = (function () {
+    return function () {
+      setTimeout(function () {
+        self.next()
+      }, delay)
+    }
+  })()
+  this.task.push(fn)
+  return this
 }
 
-_lazyMan.prototype.sleepFirst = function () {
-
+_lazyMan.prototype.sleepFirst = function (delay) {
+  var self = this
+  var fn = (function () {
+    return function () {
+      setTimeout(function () {
+        self.next()
+      }, delay)
+    }
+  })()
+  this.task.unshift(fn)
+  return this
 }
 
-_lazyMan.prototype.eat = function () {
-
+_lazyMan.prototype.eat = function (food) {
+  var fn = (function (food) {
+    return function () {
+      console.log('eat ' + food)
+    }
+  })(food)
+  this.task.push(fn)
+  return this
 }
 
-
-function lazyMan () {
-  return new _lazyMan()
+function lazyMan (name) {
+  return new _lazyMan(name)
 }
+
+lazyMan('sujunfei').sleepFirst(5000).eat('beaf')
