14ae964e048b114255fda511c67c3cb23ae9786d
sjf <1982764150@qq.com>
2017-04-06 16:43:54 +0800

    简化版本

--- javascript/deepCopy.js
+++ javascript/deepCopy.js
@@ -6,41 +6,23 @@ function isArray (arr) {
   return judgeType(arr) === '[object Array]'
 }
 
-function isObject (obj) {
-  return obj !== null && typeof obj === 'object'
-}
-
 function isStaictObject (obj) {
   return judgeType(obj) === '[object Object]'
 }
 
 function deepCopy (source, dest) {
-  if (!dest) {
-    dest = source
-    if (isArray(source)) {
-      dest = deepCopy(source, [])
-    } else if (isStaictObject(source)) {
-      dest = deepCopy(source, {})
-    }
-  } else {
-    var result
-    if (isArray(source)) {
-      dest.length = 0;
-      for (var i = 0; i < source.length; i++) {
-        result = deepCopy(source[i], null)
-        dest.push(result)
-      }
-    } else {
-      for (var prop in source) {
-        if (source.hasOwnProperty(prop)) {
-          result = deepCopy(source[prop], null)
-          dest[prop] = result
-        }
+  var copySource = isArray(source) ? [] : {}
+  for (var prop in source) {
+    if (source.hasOwnProperty(prop)) {
+      if (isArray(source[prop]) || isStaictObject(source[prop])) {
+        copySource[prop] = deepCopy(source[prop])
+      } else {
+        copySource[prop] = source[prop]
       }
     }
-    
   }
-  return dest
+
+  return copySource
 }
 
 var testObj = {
